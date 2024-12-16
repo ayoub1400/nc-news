61d6cdfe3a8900531bafe0638bffeb98f0956b2f
Ayoub Chemmach <ayoub12345@hotmail.co.uk>
2024-12-16 12:23:11 +0000

    gives all articles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from 'react'
-
 import './App.css'
+import Header from './components/Header'
+import ArticleList from './components/ArticleList'
 
 function App() {
+  return ( 
+  <>
+  <div className='App'>
+    <Header/>
+    <ArticleList/>
+  </div>
+  </>
+  )
 }
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <h1 className="Header">
+            Ayoub's News
+        </h1>
+    )
+}
+
+export default Header
